ee2192e86519ba9d6359c169671ba1afaa512784
alfredreintz <alfred.reintz@icloud.com>
2025-09-22 19:02:10 +0200

    Added comments

--- wordfreq.py
+++ wordfreq.py
@@ -1,21 +1,29 @@
+# Takes list of sentence as argument and returns the elements of the sentence as a list
 def tokenize(lines):
     words = []
+
     for line in lines:
         start = 0
-        
+
+        # Loops through the sentence        
         while start < len(line):
+            # Excludes white spaces
             while start < len(line) and line[start].isspace():
                 start += 1
              
+            # Breaks the loop if index is out of range
             if start >= len(line):
                 break
                
+            # Check type of character
             if line[start].isdigit():
                 startIndex = start
 
+                # Loops until the type of character is not repeated
                 while start < len(line) and line[start].isdigit():
                     start += 1
-   
+
+                # Adds the characters to the list
                 words.append(line[startIndex : start])
             elif line[start].isalpha(): 
                 startIndex = start
@@ -27,5 +35,6 @@ def tokenize(lines):
             else:
                 words.append(line[start])
                 start += 1
-            
+    
+    # Return the list
     return words
