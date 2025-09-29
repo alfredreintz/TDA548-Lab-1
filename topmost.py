041d1e8d8707a18c41f1839d696ea8f9d461a86b
alfredreintz <alfred.reintz@icloud.com>
2025-09-29 13:04:10 +0200

    fd

--- topmost.py
+++ topmost.py
@@ -3,13 +3,13 @@ import sys
 import urllib.request
 
 def main():
-    response = urllib.request.urlopen(sys.argv[2])
-
     fullTextFileOpen = False
 
     # Get inputs from the terminal
     stopWords = open(sys.argv[1], encoding="utf-8")
 
+
+    response = urllib.request.urlopen(sys.argv[2])
     # If the second argument starts with a http request the text is imported from the desired website
     if sys.argv[2].startswith("http://") or sys.argv[2].startswith("https://"):
         fullText = response.read().decode("utf8").splitlines()
